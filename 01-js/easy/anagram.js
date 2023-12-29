193e2fd73d791f06203a38ba182792dd97654cf3
pankaj-03 <pankajsunal045@gmail.com>
2023-12-29 21:57:54 +0530

    YOU WILL BE GREAT DEVELOPER

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,42 @@
 
 function isAnagram(str1, str2) {
 
+  //METHOD 1 O(NlogN) 
+  
+  // const t1 = str1.replace(/\s/g ,'').toLowerCase();
+  // const t2 = str2.replace(/\s/g , '').toLowerCase();
+
+  // const s1 = t1.split('').sort().join('');
+  // const s2 = t2.split('').sort().join('');
+
+  // if(s1 == s2)return true;
+
+  // return false;
+
+  //METHOD-2  O(N)
+  const s1 =  str1.replace(/\s/g,'').toLowerCase();
+  const s2 = str2.replace(/\s/g,'').toLowerCase();
+  let char = {};
+
+  if(s1.length != s2.length)return false;
+
+  for(let i=0;i<s1.length;i++){
+    char[s1[i]] = (char[s1[i]] || 0) +1 ;
+  }
+
+  for(let i=0;i<s2.length;i++){
+    if(!char[s2[i]])return false;
+
+    char[s2[i]]--;
+  }
+
+  if(Object.values(char).every(count => count === 0)){
+    return true;
+  }
+  else
+  {
+   return false;
+  }
 }
 
 module.exports = isAnagram;
