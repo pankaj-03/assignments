558ae185e1dd5cd0a9f7b4e54a0f88c0dd1e5f8d
pankaj-03 <pankajsunal045@gmail.com>
2024-01-02 14:31:35 +0530

    SUCCESS

--- week-2/01-async-js/easy/counter.js
+++ week-2/01-async-js/easy/counter.js
@@ -1,13 +1,13 @@
 //1-counter.md
 
-let count = 0;
-function counter1(count){
-    setInterval(function(){
-        console.log("count :",count);
-        count++;
-        counter1(count);
-    },1000);
-}
+// let count = 0;
+// function counter1(){
+//     setInterval(function(){
+//         console.log("count :",count);
+//         count++;
+//     },1000);
+// }
+// counter1();
 
 
 //2-counter.md
@@ -20,5 +20,26 @@ function counter1(count){
 //         counter();
 //     },1000);
 // }
-
 // counter();
+
+//3-read-from-file.md
+ let fs = require("fs");
+//read
+fs.readFile("a.txt" ,"utf-8", function(err, data){
+    console.log(data);
+})
+
+//write 
+let content = "my name is Pankaj Sunal";
+fs.writeFile("a.txt",content,function(err){
+    if(err){
+        console.error(error);
+    }
+    else{
+        console.log("commit");
+    }
+}) 
+
+fs.readFile("a.txt" ,"utf-8" , function(err , data){
+    console.log(data);
+})
