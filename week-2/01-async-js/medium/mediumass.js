d0938ef09d750c8ed54f59fb8926b8d366a7577e
pankaj-03 <pankajsunal045@gmail.com>
2024-01-02 22:13:35 +0530

    SUCCESS

--- week-2/01-async-js/medium/mediumass.js
+++ week-2/01-async-js/medium/mediumass.js
@@ -1,2 +1,39 @@
 let fs = require("fs");
 
+fs.readFile("b.txt" ,"utf-8" , function(err , data){
+    if(err){
+        console.error(err);
+    }
+    else{
+        let content = modified(data);
+        fs.writeFile("b.txt" , content , function(){
+            if(err){
+                console.log("err");
+            }
+            else{
+                console.log("commit");
+            }
+        })
+    }
+
+    fs.readFile("b.txt" , "utf-8" ,function(err , data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(data);
+        }
+    })
+})
+
+function modified(content){
+    const lines = content.split('\n');
+
+    // Remove extra spaces from each line
+    const modifiedLines = lines.map(line => line.replace(/\s+/g,' '));
+
+    // Join lines back together
+    const modifiedContent = modifiedLines.join('\n');
+
+    return modifiedContent;
+}
